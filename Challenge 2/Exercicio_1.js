9c6000bb810249d6ef18f2ac857c02ae75bb076f
Jo6o43 <joaoribeiro141516@gmail.com>
2024-10-14 00:05:55 +0100

    IDK

--- Challenge 2/Exercicio_1.js
+++ Challenge 2/Exercicio_1.js
@@ -1,18 +1,18 @@
-
+//////////////////////////////////////////////////////
 function createPlayer(name, health, strength) {
     return {
         name: name,
         health: health,
         strength: strength,
-        attack: function (player,damage) {
-            console.log(" >> " + name + " attack with " + damage + " damage.");
+        attack: function (player, damage) {
+            console.log(" >> " + name + " attack " + player.name + " with " + damage + " damage.");
             TakeDamage(player, damage);
         }
     }
 }
 
-function TakeDamage(player, damage) {~
-    console.log("=====================================");
+function TakeDamage(player, damage) {
+    equal_line();
     player.health -= damage;
     console.log(" >> " + player.name + " has been attacked.");
     console.log(" >> Damage: " + damage);
@@ -20,13 +20,25 @@ function TakeDamage(player, damage) {~
     if (player.health <= 0) {
         console.log(" >> " + player.name + " died.");
     }
+    equal_line();
+}
+
+function createEnemy (enemyType,health,strength) {
+  
+
+}
+
+
+
+function equal_line() {
     console.log("=====================================");
 }
 
-const mario = createPlayer("Mario", 100, 30);
-const luigi = createPlayer("Luigi", 100, 25);
+//main
+const player_mario = createPlayer("Mario", 100, 30);
+const player_luigi = createPlayer("Luigi", 100, 25);
 
-console.log("=====================================");
-mario.attack(luigi,30);
-mario.attack(luigi,70);
+equal_line();
+player_mario.attack(player_luigi, 30);
+player_mario.attack(player_luigi, 70);
 
