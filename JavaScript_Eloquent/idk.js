8a857346be40ef139c35830109ec9abce8996aa1
Jo6o43 <joaoribeiro141516@gmail.com>
2024-11-05 18:31:08 +0000

    Update PowerPoint

---
+++ JavaScript_Eloquent/idk.js
@@ -0,0 +1,11 @@
+
+function subtração(n, n2 = 1) {
+  let result = n - n2;
+  return result;
+}
+
+console.log(subtração(4.5));     
+//output -> 3.5
+console.log(subtração(4.5, 2));  
+//output -> 2.5
+
