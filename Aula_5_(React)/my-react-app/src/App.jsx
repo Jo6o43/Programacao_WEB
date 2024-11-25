041b1bb95e94bf6d64b43fcc7d84248ab3cf3bfe
Jo6o43 <joaoribeiro141516@gmail.com>
2024-11-25 13:24:16 +0000

    idk

--- Aula_5_(React)/my-react-app/src/App.jsx
+++ Aula_5_(React)/my-react-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       {/*<LoginScreen/>*/}
-      <RegisterScreen/>
+      <RegisterScreen />
 
     </>
   );
--- JavaScript_Eloquent/idk.js
+++ JavaScript_Eloquent/idk.js
@@ -1,11 +1,13 @@
 
-function subtração(n, n2 = 1) {
-  let result = n - n2;
-  return result;
+function potência(base, expoente) {
+  if (expoente == 0) {
+    return 1; 
+  } else {
+    console.log(base, expoente);
+    return base * potência(base, expoente - 1); 
+  }
 }
 
-console.log(subtração(4.5));     
-//output -> 3.5
-console.log(subtração(4.5, 2));  
-//output -> 2.5
+console.log(potência(2, 3)); 
+//output -> 8
 
